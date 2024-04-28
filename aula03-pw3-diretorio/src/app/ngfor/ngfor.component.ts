de486873e07a0fafa7f4aaa018c0b0b68df2772c
END <125739521+Arandy-END@users.noreply.github.com>
2024-04-28 13:21:11 -0300

    Add files via upload

---
+++ aula03-pw3-diretorio/src/app/ngfor/ngfor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngfor',
+  templateUrl: './ngfor.component.html',
+  styleUrls: ['./ngfor.component.css']
+})
+export class NgforComponent {
+
+  Professor: string[] = ["Marion", "Rogerio", "Adriano", "Marcos"]
+}
